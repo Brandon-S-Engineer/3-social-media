45cf06d8bddbe415e3a16fb99d2fff719e17e894
Brandon Soria <brandon.se.25@gmail.com>
2024-05-10 00:15:27 -0600

    u

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,37 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; // To send Web Token
 import User from '../models/User.js';
+
+/* ------------------------------ Register User ----------------------------- */
+// req = request body from the frontend
+// res = response that we'll send to the frontend
+export const register = async (req, res) => {
+  try {
+    // Destructuring properties sent by frontend
+    const { firstName, lastName, email, password, picturePath, friends, location, occupation } = req.body;
+
+    // Generate a salt for hashing
+    const salt = await bcrypt.genSalt();
+    // Hash the password with the generated salt
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    // Send newly created user details to the frontend with status 201
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
