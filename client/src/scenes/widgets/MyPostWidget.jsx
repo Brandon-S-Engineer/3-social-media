3b47042e3a77be75cd023d72fd4189cd8597b419
Brandon Soria <brandon.se.25@gmail.com>
2024-06-22 10:37:22 -0600

    u

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -44,12 +44,93 @@ const MyPostWidget = () => {
   return (
     <WidgetWrapper>
       <FlexBetween gap='1.5rem'>
-        <UserImage image={picturePath}>
-          <InputBase
-            placeholder="What's on your mind..."
-            onChange={(e) => setPost(e.target.value)}
-          />
-        </UserImage>
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder="What's on your mind..."
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+          sx={{
+            width: '100%',
+            backgroundColor: palette.neutral.light,
+            borderRadius: '2rem',
+            padding: '1rem 2rem',
+          }}
+        />
+      </FlexBetween>
+
+      {/* Add Image */}
+      {isImage && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius='5px'
+          mt='1rem'
+          p='1rem'>
+          <Dropzone
+            acceptedFiles='.jpg,.jpeg,.png'
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}>
+            {({ getRootProps, getInputProps }) => (
+              <FlexBetween>
+                <Box
+                  {...getRootProps()}
+                  border={`2px dashed ${palette.primary.main}`}
+                  p='1rem'
+                  width='100%'
+                  sx={{ '&:hover': { cursor: 'pointer' } }}>
+                  <input {...getInputProps()} />
+                  {!image ? (
+                    <p>Add Image Here</p>
+                  ) : (
+                    <FlexBetween>
+                      <Typography>{image.name}</Typography>
+                      <EditOutlined />
+                    </FlexBetween>
+                  )}
+                </Box>
+                {image && (
+                  // Remove Image
+                  <IconButton
+                    onClick={() => setImage(null)}
+                    sx={{ width: '15%' }}>
+                    <DeleteOutlined />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            )}
+          </Dropzone>
+        </Box>
+      )}
+
+      <Divider sx={{ margin: '1.25rem 0' }} />
+
+      <FlexBetween>
+        {/* Update image Dropzone state */}
+        <FlexBetween
+          gap='0.25rem'
+          onClick={() => setIsImage(!isImage)}>
+          <ImageOutlined sx={{ color: mediumMain }} />
+          <Typography
+            color={mediumMain}
+            sx={{ '&:hover': { cursor: 'pointer', color: 'medium' } }}>
+            Image
+          </Typography>
+        </FlexBetween>
+
+        {isNonMobileScreens ? (
+          <>
+            <FlexBetween gap='0.25rem'>
+              <GifBoxOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Clip</Typography>
+            </FlexBetween>
+
+            <FlexBetween gap='0.25rem'>
+              <GifBoxOutlined sx={{ color: mediumMain }} />
+              <Typography color={mediumMain}>Clip</Typography>
+            </FlexBetween>
+          </>
+        ) : (
+          <FlexBetween></FlexBetween>
+        )}
       </FlexBetween>
     </WidgetWrapper>
   );
