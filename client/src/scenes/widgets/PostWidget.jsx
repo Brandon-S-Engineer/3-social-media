fc30f01d195ac119ca6c6da26a87b409c23ba9c2
Brandon <fortuneblue25@gmail.com>
2025-01-02 17:30:59 -0600

    u

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -61,7 +61,7 @@ const PostWidget = ({ postId, postUserId, name, description, location, picturePa
           height='auto'
           alt='post'
           style={{ borderRadius: '0.75rem', marginTop: '0.75rem' }}
-          src={`http://localhost:3001/assets/${picturePath}`}
+          src={`https://three-social-media.onrender.com/assets/${picturePath}`}
         />
       )}
 
